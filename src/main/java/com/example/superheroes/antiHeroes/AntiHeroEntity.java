fe98a6c1f9c0b02f701bfc687f09e39f6ca61374
Manni Basi <manni.basi@gmail.com>
2023-10-14 04:51:49 +0100

    Created initial Anti Hero entity

---
+++ src/main/java/com/example/superheroes/antiHeroes/AntiHeroEntity.java
@@ -0,0 +1,29 @@
+package com.example.superheroes.antiHeroes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+public class AntiHeroEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "UUID")
+    @Column(nullable = false, updatable = false)
+    private UUID id;
+    @NotNull(message = "First name is required")
+    private String firstName;
+    private String lastName;
+    private String house;
+    private String knownAs;
+    private String createdAt = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss z").format(new Date());
+}
