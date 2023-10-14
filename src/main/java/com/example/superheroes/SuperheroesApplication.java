e003b399455d8c8867a8992689ae62381572d33a
Manni Basi <manni.basi@gmail.com>
2023-10-14 05:33:26 +0100

    Added 'convert to' mapper methods

--- src/main/java/com/example/superheroes/SuperheroesApplication.java
+++ src/main/java/com/example/superheroes/SuperheroesApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SuperheroesApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SuperheroesApplication.class, args);
     }
-
 }
--- src/main/java/com/example/superheroes/antiHero/controller/AntiHeroController.java
+++ src/main/java/com/example/superheroes/antiHero/controller/AntiHeroController.java
@@ -1,5 +1,7 @@
 package com.example.superheroes.antiHero.controller;
 
+import com.example.superheroes.antiHero.dto.AntiHeroDto;
+import com.example.superheroes.antiHero.entity.AntiHeroEntity;
 import com.example.superheroes.antiHero.service.AntiHeroService;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -12,4 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AntiHeroController {
     private final AntiHeroService service;
     private final ModelMapper mapper;
+
+    private AntiHeroDto convertToDto(AntiHeroEntity entity) {
+        return mapper.map(entity, AntiHeroDto.class);
+    }
+
+    private AntiHeroEntity convertToEntity(AntiHeroDto dto) {
+        return mapper.map(dto, AntiHeroEntity.class);
+    }
 }
