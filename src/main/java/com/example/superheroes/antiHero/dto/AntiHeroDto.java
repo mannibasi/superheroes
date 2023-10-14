9d9bb57da02998bdef79340cd191fc95be0d41cc
Manni Basi <manni.basi@gmail.com>
2023-10-14 04:58:37 +0100

    Added DTO for Anti Hero with validation to match constraint on entity

---
+++ src/main/java/com/example/superheroes/antiHero/dto/AntiHeroDto.java
@@ -0,0 +1,17 @@
+package com.example.superheroes.antiHero.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+
+@Getter
+@Setter
+public class AntiHeroDto {
+    private UUID id;
+    @NotNull(message = "First name is required")
+    private String firstName;
+    private String lastName;
+    private String house;
+    private String knownAs;
+}
