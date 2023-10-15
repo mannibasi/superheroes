adb2ee35821c2bab6c4e1fd9db0068a6a119fc69
= <manni.basi@gmail.com>
2023-10-15 17:25:33 +0100

    Added pagination for 'all heroes'

--- src/main/java/com/example/superheroes/antiHero/controller/AntiHeroController.java
+++ src/main/java/com/example/superheroes/antiHero/controller/AntiHeroController.java
@@ -5,6 +5,7 @@ import com.example.superheroes.antiHero.entity.AntiHeroEntity;
 import com.example.superheroes.antiHero.service.AntiHeroService;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -23,10 +24,12 @@ public class AntiHeroController {
     private final ModelMapper mapper;
 
     @GetMapping
-    public List<AntiHeroDto> getAntiHeroes() {
+    public List<AntiHeroDto> getAntiHeroes(Pageable pageable) {
+        int toSkip = pageable.getPageSize() * pageable.getPageNumber();
         var antiHeroList = StreamSupport
                 .stream(service.findAllAntiHeroes().spliterator(),
                         false)
+                .skip(toSkip).limit(pageable.getPageSize())
                 .collect(Collectors.toList());
         return antiHeroList
                 .stream()
