48931bb651b68a992c1078dfadea9bf97cec5998
= <manni.basi@gmail.com>
2023-10-28 15:31:34 +0100

    Added H2 (test) properties and service test

---
+++ src/test/java/com/example/superheroes/antiHero/h2/service/AntiHeroH2ServiceTest.java
@@ -0,0 +1,35 @@
+package com.example.superheroes.antiHero.h2.service;
+
+import com.example.superheroes.antiHero.entity.AntiHeroEntity;
+import com.example.superheroes.antiHero.repository.AntiHeroRepository;
+import com.example.superheroes.antiHero.service.AntiHeroService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+public class AntiHeroH2ServiceTest {
+    @Autowired
+    private AntiHeroRepository repo;
+    private AntiHeroService service;
+
+    @BeforeEach
+    public void setUp() {
+        service = new AntiHeroService(repo);
+    }
+
+    @Test
+    public void shouldFindAllAntiHeroes() {
+        AntiHeroEntity antiHero = new AntiHeroEntity();
+        antiHero.setFirstName("Eddie");
+        antiHero.setLastName("Brock");
+        antiHero.setHouse("MCU");
+        service.addAntiHero(antiHero);
+        Iterable<AntiHeroEntity> antiHeroList = service.findAllAntiHeroes();
+        AntiHeroEntity savedAntiHero = antiHeroList.iterator().next();
+        assertThat(savedAntiHero).isNotNull();
+    }
+}
