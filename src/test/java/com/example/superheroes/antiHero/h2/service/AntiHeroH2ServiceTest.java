1370cbd320c38f0734ccbe3e61c14541401073f6
= <manni.basi@gmail.com>
2023-11-11 17:42:05 +0000

    Added delete anti hero test

--- src/test/java/com/example/superheroes/antiHero/h2/service/AntiHeroH2ServiceTest.java
+++ src/test/java/com/example/superheroes/antiHero/h2/service/AntiHeroH2ServiceTest.java
@@ -3,12 +3,17 @@ package com.example.superheroes.antiHero.h2.service;
 import com.example.superheroes.antiHero.entity.AntiHeroEntity;
 import com.example.superheroes.antiHero.repository.AntiHeroRepository;
 import com.example.superheroes.antiHero.service.AntiHeroService;
+import com.example.superheroes.exception.NotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.UUID;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
 public class AntiHeroH2ServiceTest {
@@ -56,5 +61,22 @@ public class AntiHeroH2ServiceTest {
         service.updateAntiHero(savedAntiHero.getId(),savedAntiHero);
         AntiHeroEntity foundAntiHero = service.findAntiHeroById(savedAntiHero.getId());
         assertThat(foundAntiHero.getHouse()).isEqualTo("DC");
+    }
+
+    @Test
+    public void shouldDeleteAntiHero() {
+        assertThrows(NotFoundException.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                AntiHeroEntity antiHero = new AntiHeroEntity();
+                antiHero.setFirstName("Eddie");
+                antiHero.setLastName("Brock");
+                antiHero.setHouse("MCU");
+                AntiHeroEntity savedAntiHero = service.addAntiHero(antiHero);
+                service.removeAntiHeroById(savedAntiHero.getId());
+                AntiHeroEntity foundAntiHero = service.findAntiHeroById(savedAntiHero.getId());
+                assertThat(foundAntiHero).isNull();
+            }
+        });
     }
 }
