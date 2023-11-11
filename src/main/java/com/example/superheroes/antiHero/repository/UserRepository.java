f3c48c97afe4e7b1e224d22c6ee087dce7284a94
= <manni.basi@gmail.com>
2023-11-11 20:19:30 +0000

    Added/implemented user DTO, entity and repo - tests back to green

--- src/main/java/com/example/superheroes/antiHero/repository/UserRepository.java
+++ src/main/java/com/example/superheroes/antiHero/repository/UserRepository.java
@@ -2,9 +2,20 @@ package com.example.superheroes.antiHero.repository;
 
 import com.example.superheroes.antiHero.entity.AntiHeroEntity;
 import com.example.superheroes.antiHero.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.UUID;
 
-public interface UserRepository extends CrudRepository<UserEntity, UUID> {
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+    @Query(
+            "" +
+                    "SELECT CASE WHEN COUNT(u) > 0 THEN " +
+                    "TRUE ELSE FALSE END " +
+                    "FROM UserEntity u " +
+                    "WHERE u.email = ?1"
+    )
+    Boolean selectExistsEmail(String email);
+    UserEntity findByEmail(String email);
 }
--- src/test/java/com/example/superheroes/user/repository/UserRepositoryTest.java
+++ src/test/java/com/example/superheroes/user/repository/UserRepositoryTest.java
@@ -1,11 +1,24 @@
 package com.example.superheroes.user.repository;
 
+import com.example.superheroes.antiHero.entity.UserEntity;
 import com.example.superheroes.antiHero.repository.UserRepository;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataJpaTest
 public class UserRepositoryTest {
     @Autowired
     private UserRepository underTest;
+
+    @Test
+    public void itShouldCheckWhenUserEmailExists() {
+        String email = "test@test.com";
+        UserEntity userEntity = new UserEntity(email, "3142523");
+        underTest.save(userEntity);
+        boolean expected = underTest.selectExistsEmail(email);
+        assertThat(expected).isTrue();
+    }
 }
